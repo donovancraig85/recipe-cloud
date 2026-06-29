8b173debac00530b93e39572e4bf88ae9660601d
donovancraig85 <109382053+donovancraig85@users.noreply.github.com>
2026-06-29 13:31:44 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ import {
 --------------------------------------------------------- */
 const firebaseConfig = {
   apiKey: "AIzaSyD-ZVROybS5c3O6kJhe8LVcXNZ0KbYTmvg",
-  authDomain: "recipes-83727.firebaseapp.com",
-  projectId: "recipes-83727",
-  storageBucket: "recipes-83727.firebasestorage.app",
-  messagingSenderId: "97445031584",
-  appId: "1:97445031584:web:a463b119a272531f51a3c5",
+  authDomain: "recipes-cloud-6f09d.firebaseapp.com",
+  projectId: "recipes-cloud-6f09d",
+  storageBucket: "recipes-cloud-6f09d.firebasestorage.app",
+  messagingSenderId: "987741844532",
+  appId: "1:987741844532:web:a463b119a272531f51a3c5",
   measurementId: "G-4LERX7EWB7"
 };
 
